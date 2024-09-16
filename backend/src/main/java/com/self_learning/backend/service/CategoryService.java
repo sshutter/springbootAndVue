2d5982a721b441464e7a8b1b9f88e6811951a0ce
shutter <6331335921@student.chula.ac.th>
2024-09-16 15:08:41 +0700

    Complete category APIs

--- backend/src/main/java/com/self_learning/backend/service/CategoryService.java
+++ backend/src/main/java/com/self_learning/backend/service/CategoryService.java
@@ -1,11 +1,13 @@
 package com.self_learning.backend.service;
 
 import com.self_learning.backend.model.Category;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.self_learning.backend.repository.CategoryRepo;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class CategoryService {
@@ -20,4 +22,24 @@ public class CategoryService {
         return categoryRepo.findAll();
     }
 
+    public void editCategory(int categoryId, Category updateCategory) {
+        Optional<Category> categoryOptional = categoryRepo.findById(categoryId);
+        if (categoryOptional.isPresent()) {
+            Category category = categoryOptional.get();
+            // Handle if null value
+            if(updateCategory.getCategoryName() != null) {
+                category.setCategoryName(updateCategory.getCategoryName());
+            }
+            if(updateCategory.getDescription() != null) {
+                category.setDescription(updateCategory.getDescription());
+            }
+            if(updateCategory.getImageUrl() != null) {
+                category.setImageUrl(updateCategory.getImageUrl());
+            }
+            categoryRepo.save(category);
+        } else {
+            throw new EntityNotFoundException("Category with ID " + categoryId + " not found");
+        }
+
+    }
 }
